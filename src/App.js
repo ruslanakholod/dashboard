5799ba2c98de9dd8016cb39032b2775db12b7f8a
Ruslana Kholod <RuslanaKholod@gmail.com>
2019-07-12 13:30:53 +0300

    Add button which allows to add a new card

--- src/App.js
+++ src/App.js
@@ -35,6 +35,16 @@ class App extends React.Component {
         document.removeEventListener("keydown", this.handleKeyPress);
     }
 
+    addNewItem = () => {
+        const newItems = this.state.items.map(item => ({...item, id: item.id + 1 }));
+        const item = {
+            id: 1,
+            title: '',
+            img: '',
+        };
+        newItems.unshift(item);
+        this.setState({items: newItems});
+    };
 
     handleKeyPress = event => {
 
@@ -100,9 +110,10 @@ class App extends React.Component {
 
         }
 
-    }
+    };
 
     handleChangeInput = (event, id) => {
+        event.stopPropagation();
         const newItems = this.state.items.map((item) => item.id === id ? {...item, title: event.target.value} : item);
         this.setState({items: newItems});
     };
@@ -122,9 +133,27 @@ class App extends React.Component {
             />
         ));
 
+        const Button = (
+            <button onClick={this.addNewItem}>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                     width="40px" height="40px" viewBox="0 0 612 612">
+                    <path d="M306,0C136.992,0,0,136.992,0,306s136.992,306,306,306s306-137.012,306-306S475.008,0,306,0z M306,573.75
+                                C158.125,573.75,38.25,453.875,38.25,306C38.25,158.125,158.125,38.25,306,38.25c147.875,0,267.75,119.875,267.75,267.75
+                                C573.75,453.875,453.875,573.75,306,573.75z M420.75,286.875h-95.625V191.25c0-10.557-8.568-19.125-19.125-19.125
+                                c-10.557,0-19.125,8.568-19.125,19.125v95.625H191.25c-10.557,0-19.125,8.568-19.125,19.125c0,10.557,8.568,19.125,19.125,19.125
+                                h95.625v95.625c0,10.557,8.568,19.125,19.125,19.125c10.557,0,19.125-8.568,19.125-19.125v-95.625h95.625
+                                c10.557,0,19.125-8.568,19.125-19.125C439.875,295.443,431.307,286.875,420.75,286.875z"
+                          fill='white'/>
+                </svg>
+            </button>
+        );
+
         return (
             <div>
                 <div style={container}>
+                    <div className={styles.dashboard__button}>
+                        {Button}
+                    </div>
                     <div className={styles.dashboard__wrapper}>{dashboardItems}</div>
                 </div>
             </div>
@@ -150,13 +179,24 @@ injectGlobal`
 const container = {
     maxWidth: "1550px",
     margin: "0 auto",
-    padding: "0 25px"
+    padding: "0 15px"
 };
 
 const styles = {
     dashboard__wrapper: css`
     display: flex;
     flex-wrap: wrap;
-    padding: 50px 0;
-  `
+    padding: 30px 0 50px 0;
+  `,
+    dashboard__button: css`
+    text-align: right;
+    
+    button {
+    height: 40px;
+    margin: 20px 25px 0 0;
+    border: 0;
+    background: none;
+    cursor: pointer;
+    }
+    `
 };
